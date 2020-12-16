45e31286dfc7bcd985dd3bce8273d03196c93197
Mikhail <16112000m@gmail.com>
2020-12-16 18:03:50 +0300

    Some comments

--- Basic projects/helloWorld.py
+++ Basic projects/helloWorld.py
@@ -11,23 +11,30 @@ class HelloWorldWindow(QtWidgets.QWidget):
         """
         super().__init__(parent)
 
+        # create text label
         self.label = QtWidgets.QLabel("Hello, world!")
+        # set text alignment to center
         self.label.setAlignment(QtCore.Qt.AlignHCenter)
         # & used for alt+c hotkey
         self.btnQuit = QtWidgets.QPushButton("&Close window")
 
+        # create vertical layout
         self.vbox = QtWidgets.QVBoxLayout()
+        # and add our widgets
         self.vbox.addWidget(self.label)
         self.vbox.addWidget(self.btnQuit)
 
+        # set vertical layout to window
         self.setLayout(self.vbox)
 
+        # quit button listener
         self.btnQuit.clicked.connect(QtWidgets.qApp.quit)
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
+    # create window
     window = HelloWorldWindow()
     window.setWindowTitle("First QT programm")
     window.resize(300, 70)
--- Basic projects/helloWorldExtra.py
+++ Basic projects/helloWorldExtra.py
@@ -7,17 +7,23 @@ class HelloWorldDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # get window
         self.myWidget = helloWorld.HelloWorldWindow()
+        # and set margins to it
         self.myWidget.vbox.setContentsMargins(0, 0, 0, 0)
 
+        # add new button to edit text
         self.button = QtWidgets.QPushButton("&Edit text")
 
+        # create vertical layout and add our widgets
         self.mainBox = QtWidgets.QVBoxLayout()
         self.mainBox.addWidget(self.myWidget)
         self.mainBox.addWidget(self.button)
 
+        # set vertical layout to window
         self.setLayout(self.mainBox)
 
+        # quit button listener
         self.button.clicked.connect(self.on_clicked)
 
     def on_clicked(self):
@@ -28,6 +34,7 @@ class HelloWorldDialog(QtWidgets.QDialog):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
+    # create window
     window = HelloWorldDialog()
     window.setWindowTitle("Second QT programm")
     window.resize(300, 100)
